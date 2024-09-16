ba93a725ad4ccc70ad98a4d8de500dd92cf5ba48
Pavel Damyanov <pavel_dam@outlook.com>
2024-09-16 15:19:50 +0300

    Adding the solution to problem 3 from the exam: 26June2021

---
+++ Java Advanced/_Exams/Exam_26June2021/P03_GroomingSalon/groomingSalon/Pet.java
@@ -0,0 +1,42 @@
+package _Exams.Exam_26June2021.P03_GroomingSalon.groomingSalon;
+
+public class Pet {
+    private String name;
+    private int age;
+    private String owner;
+
+    public Pet(String name, int age, String owner) {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d - (%s)",name,age,owner);
+    }
+}
